a7e23973ad6bddb0149a56d54833f8ff3ac65498
Bitel Mac <mibitelios2022@gmail.com>
2022-11-22 14:39:49 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Bitel-Dbus on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
